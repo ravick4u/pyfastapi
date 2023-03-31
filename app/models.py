d789bd23e92c102e0ac844226d0ff98e8977302b
Ravi Khambhati <ravisoftltd@gmail.com>
2023-03-31 16:02:17 -0400

    Created timestamp column

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String, text
 
 
 class Post(Base):
@@ -8,4 +8,6 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='t', nullable=False)
+    created = Column(TIMESTAMP(timezone=True),
+                     nullable=False, server_default=text('now()'))
